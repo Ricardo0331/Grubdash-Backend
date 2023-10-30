aac97fadd0ae6f52198be3964ed25f2b2646fae9
Ricardo0331 <ricardose0419@gmail.com>
2023-10-30 14:06:55 -0500

    updated both dish controller and router to include routes for / and a middleware check to know if either controller and orders exist

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,9 +1,26 @@
 const path = require("path");
-
-// Use the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"));
-
-// Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
+//Middleware to check if dish exist
+function dishExists(req, res, next) {
+    const { dishId } = req.params;
+    const foundDish = dishes.find((dish) => dish.id === dishId); 
+    if (foundDish) {
+        res.locals.dish = foundDish
+        return next();
+    }
+    next({ status: 404, message: `Dish id not found: ${dishId}` });
+};
+
+
+//List all dishes
+function list(req,res) {
+    res.json ({ data: dishes });
+};
+
+
+module.exports = {
+    list,
+
+};
