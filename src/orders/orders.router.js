d493ba9c899d63e588b25d55dbfdd187fa77359f
Ricardo0331 <ricardose0419@gmail.com>
2023-10-30 15:03:06 -0500

    Forgot to wire handlers to router, added them now

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -4,5 +4,14 @@ const controller = require("./orders.controller");
 
 router.route("/").get(controller.list);
 
+router
+    .router("/")
+    .get(controller.list)
+    .post(controller.create);
+
+router
+    .route("/:orderId")
+    .get(controller.orderExist, controller.read)
+
 
 module.exports = router;
